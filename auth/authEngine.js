303a1b22f0e1c82f5a7e25fd1cffaf5e98ee8883
chrisochei20-star <chrisochei20@gmail.com>
2026-05-16 19:50:57 +0100

    ccweb backend ready for deploy

--- auth/authEngine.js
+++ auth/authEngine.js
@@ -3,7 +3,7 @@
  */
 
 const crypto = require("crypto");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const authStore = require("./authStore");
 const jwtTokens = require("./jwtTokens");
 const totp = require("./totp");
